2b1d03d03b7996783ac16777de35a8f86cd071fd
carryminati7 <shaikhrehan63892@gmail.com>
2024-11-26 15:12:26 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+
+
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7273137374:AAFwZkObpjNYkAq5YCvyxU9qfINDb4hWnd4")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@shaikhrehan786590")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5946419615")
+
+
+
+
+
+
+
+  
